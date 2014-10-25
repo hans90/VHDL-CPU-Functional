9ec9741910efcb6201f6c62b0d86e8fc9407f825
x5444 <accounts@x5444.com>
2014-10-25 12:24:34 +0200

    Fixed Syntax Errors in Packages.

--- cpu_defs_pack.vhd
+++ cpu_defs_pack.vhd
@@ -1,3 +1,24 @@
 package CPU_DEFS_PACK is
-begin
+constant bus_width      : natural := 12;        --2.1.1
+ constant data_width     : natural := bus_width; -- 2.1.2
+ constant addr_width     : natural := bus_width; -- 2.1.3
+ constant reg_addr_width : natural := 2;         -- do not change
+ constant opcode_width   : natural := 6;         -- do not change
+ subtype data_type is
+    natural range 0 to 2**data_width-1;
+ subtype addr_type is
+    natural range 0 to 2**addr_width-1;
+ subtype reg_addr_type is
+    natural range 0 to 2**reg_addr_width-1;
+ subtype opcode_type is
+    natural range 0 to 2**opcode_width-1;
+ type reg_type is array(reg_addr_type) of data_type;
+ type mem_type is array(addr_type) of data_type;
+ constant code_nop  : opcode_type:= 0;       -- 3.5.1
+ constant code_stop : opcode_type:= 1;       -- 3.5.2
+
+procedure EXEC_ADDC ( A,B      : in  data_type;
+                      CI       : in  boolean := FALSE;
+                      R        : out data_type;
+                      Z,CO,N,O : out boolean   );
 end CPU_DEFS_PACK;
--- cpu_defs_pack_body.vhd
+++ cpu_defs_pack_body.vhd
@@ -1,3 +1,46 @@
-package body CPU_DEFS_PACK_BODY of CPU_DEFS_PACK is
+package body CPU_DEFS_PACK is
+
+
+procedure EXEC_ADDC ( A,B      : in  data_type;
+                      CI       : in  boolean := FALSE;
+                      R        : out data_type;
+                      Z,CO,N,O : out boolean   ) is
+variable T: integer := A+B+Boolean'Pos( CI );
+variable A_s, B_s, T_s : integer;
 begin
-end CPU_DEFS_PACK_BODY;
+   if A >= 2**(data_width-1) then
+      A_s := A - 2**(data_width);
+   else
+      A_s := A;
+   end if;
+   if B >= 2**(data_width-1) then
+      B_s := B - 2**(data_width);
+   else
+      B_s := B;
+   end if;
+   T_s := A_s+B_s+Boolean'Pos( CI );
+   if T >= 2**data_width then
+      R := T - 2**data_width;
+      CO := TRUE;
+   else
+      R := T;
+      CO := FALSE;
+   end if;
+   if T mod 2**data_width = 0 then
+      Z := TRUE;
+   else
+      Z := FALSE;
+   end if;
+   if T_s < 0 then
+      N := TRUE;
+   else
+      N := FALSE;
+   end if;
+   if (T_s < -2**(data_width-1)) or (T_s >= 2**(data_width-1)) then
+      O := TRUE;
+   else
+      O := FALSE;
+   end if; 
+end EXEC_ADDC;
+
+end CPU_DEFS_PACK;
