304a7dc544f5045ad19eaa52b9b81c41ec60f0e2
x5444 <accounts@x5444.com>
2014-10-27 15:36:44 +0100

    - Implemented all Bitshift and Rotation functions - Added SUB TB to PA Project

--- cpu_defs_pack.vhd
+++ cpu_defs_pack.vhd
@@ -1,29 +1,75 @@
 package CPU_DEFS_PACK is
-constant bus_width      : natural := 12;        --2.1.1
- constant data_width     : natural := bus_width; -- 2.1.2
- constant addr_width     : natural := bus_width; -- 2.1.3
- constant reg_addr_width : natural := 2;         -- do not change
- constant opcode_width   : natural := 6;         -- do not change
- subtype data_type is
-    natural range 0 to 2**data_width-1;
- subtype addr_type is
-    natural range 0 to 2**addr_width-1;
- subtype reg_addr_type is
-    natural range 0 to 2**reg_addr_width-1;
- subtype opcode_type is
-    natural range 0 to 2**opcode_width-1;
- type reg_type is array(reg_addr_type) of data_type;
- type mem_type is array(addr_type) of data_type;
- constant code_nop  : opcode_type:= 0;       -- 3.5.1
- constant code_stop : opcode_type:= 1;       -- 3.5.2
 
-procedure EXEC_ADDC ( A,B      : in  data_type;
-                      CI       : in  boolean := FALSE;
-                      R        : out data_type;
-                      Z,CO,N,O : out boolean   );
+   constant bus_width      : natural := 12;        --2.1.1
+   constant data_width     : natural := bus_width; -- 2.1.2
+   constant addr_width     : natural := bus_width; -- 2.1.3
 
-procedure EXEC_SUBC ( A,B      : in  data_type;
-                      CI       : in  boolean := FALSE;
-                      R        : out data_type;
-                      Z,CO,N,O : out boolean   );
+   constant reg_addr_width : natural := 2;         -- do not change
+   constant opcode_width   : natural := 6;         -- do not change
+
+   subtype data_type is
+      natural range 0 to 2**data_width-1;
+
+   subtype addr_type is
+      natural range 0 to 2**addr_width-1;
+
+   subtype reg_addr_type is
+      natural range 0 to 2**reg_addr_width-1;
+
+   subtype opcode_type is
+      natural range 0 to 2**opcode_width-1;
+
+   type reg_type is array(reg_addr_type) of data_type;
+
+   type mem_type is array(addr_type) of data_type;
+
+   constant code_nop  : opcode_type:= 0;       -- 3.5.1
+   constant code_stop : opcode_type:= 1;       -- 3.5.2
+
+
+
+   -----------------------------------------------------
+   --
+   -- BIT SHIFT/ROTATIONs
+   --
+   -----------------------------------------------------
+
+   -- Logical Shift Left
+   procedure EXEC_SLL  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   );
+
+   -- Logical Shift Right
+   procedure EXEC_SRL  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   );
+
+   -- Arithmetic Shift Right
+   procedure EXEC_SRA  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   );
+
+   -- Rotate Right
+   procedure EXEC_ROR  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   );
+
+   -- Rotate Right through Carry
+   procedure EXEC_RORC ( A        : in  data_type;
+                         CI       : in  boolean;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   );
+
+   -- Rotate Left
+   procedure EXEC_ROL  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   );
+
+   -- Rotate Left through Carry
+   procedure EXEC_ROLC ( A        : in  data_type;
+                         CI       : in  boolean;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   );
+
+
 end CPU_DEFS_PACK;
--- cpu_defs_pack_body.vhd
+++ cpu_defs_pack_body.vhd
@@ -1,88 +1,315 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+
+
+
 package body CPU_DEFS_PACK is
 
 
-procedure EXEC_ADDC ( A,B      : in  data_type;
-                      CI       : in  boolean := FALSE;
-                      R        : out data_type;
-                      Z,CO,N,O : out boolean   ) is
-variable T: integer := A+B+Boolean'Pos( CI );
-variable A_s, B_s, T_s : integer;
-begin
-   if A >= 2**(data_width-1) then
-      A_s := A - 2**(data_width);
-   else
-      A_s := A;
-   end if;
-   if B >= 2**(data_width-1) then
-      B_s := B - 2**(data_width);
-   else
-      B_s := B;
-   end if;
-   T_s := A_s+B_s+Boolean'Pos( CI );
-   if T >= 2**data_width then
-      R := T - 2**data_width;
-      CO := TRUE;
-   else
-      R := T;
-      CO := FALSE;
-   end if;
-   if T mod 2**data_width = 0 then
-      Z := TRUE;
-   else
-      Z := FALSE;
-   end if;
-   if T_s < 0 then
-      N := TRUE;
-   else
-      N := FALSE;
-   end if;
-   if (T_s < -2**(data_width-1)) or (T_s >= 2**(data_width-1)) then
-      O := TRUE;
-   else
-      O := FALSE;
-   end if; 
-end EXEC_ADDC;
 
-procedure EXEC_SUBC ( A,B      : in  data_type;
-                      CI       : in  boolean := FALSE;
-                      R        : out data_type;
-                      Z,CO,N,O : out boolean   ) is
-variable T: integer := A-B-Boolean'Pos( CI );
-variable A_s, B_s, T_s : integer;
-begin
-   if A >= 2**(data_width-1) then
-      A_s := A - 2**(data_width);
-   else
-      A_s := A;
-   end if;
-   if B >= 2**(data_width-1) then
-      B_s := B - 2**(data_width);
-   else
-      B_s := B;
-   end if;
-   T_s := A_s-B_s-Boolean'Pos( CI );
-   if T < 0 then
-      R := T + 2**data_width;
-      CO := TRUE;
-   else
-      R := T;
-      CO := FALSE;
-   end if;
-   if T mod 2**data_width = 0 then
-      Z := TRUE;
-   else
-      Z := FALSE;
-   end if;
-   if T_s < 0 then
-      N := TRUE;
-   else
-      N := FALSE;
-   end if;
-   if (T_s < -2**(data_width-1)) or (T_s >= 2**(data_width-1)) then
-      O := TRUE;
-   else
-      O := FALSE;
-   end if; 
-end EXEC_SUBC;
+   -----------------------------------------------------
+   --
+   -- BIT SHIFT/ROTATIONs
+   --
+   -- These instructions provide for operations to shift or rotate registers by
+   -- one bit.
+   --
+   -- Implementations by Julian Leyh
+   -- Testbenches by Johannes Linder, Hassib Belhaj
+   --
+   -----------------------------------------------------
+
+
+
+   -- Logical Shift Left
+   procedure EXEC_SLL  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   ) is
+
+      constant A_u   : unsigned (DATA_WIDTH-1 downto 0) := to_unsigned(A,DATA_WIDTH);
+      variable R_b   : integer range 0 to 2**DATA_WIDTH-1 := 0;
+
+   begin
+
+      R_b := to_integer(A_u(DATA_WIDTH-2 downto 0) & '0');
+
+      if R_b = 0 then -- ->3.2.4.1
+         Z := true;
+      else
+         Z := false;
+      end if;
+
+      if A_u(DATA_WIDTH-1) = '1' then -- ->3.2.5.4
+         CO := true;
+      else
+         CO := false;
+      end if;
+
+      -- ->3.2.6.2
+      -- Since A_u(DATA_WIDTH-2) is MSB of R we'll just use A_u(DATA_WIDTH-2)
+      -- This should be faster and it's less code
+      if A_u(DATA_WIDTH-2) = '1' then
+         N := true;
+      else
+         N := false;
+      end if;
+      
+      -- ->3.2.7.2
+      -- Since A_u(DATA_WIDTH-2) is MSB of R and A_u(DATA_WIDTH-1) is MSB of we
+      -- we'll just use those two. This should be faster and it's less code
+      if A_u(DATA_WIDTH-2) /= A_u(DATA_WIDTH-1) then
+         O := true;
+      else
+         O := false;
+      end if;
+
+      R := R_b;
+
+   end procedure; -- Julian Leyh
+
+
+
+   -- Logical Shift Right
+   procedure EXEC_SRL  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   ) is
+
+      constant A_u   : unsigned (DATA_WIDTH-1 downto 0) := to_unsigned(A,DATA_WIDTH);
+      variable R_b   : integer range 0 to 2**DATA_WIDTH-1 := 0;
+
+   begin
+
+      R_b := to_integer('0' & A_u(DATA_WIDTH-1 downto 1));
+
+      if R_b = 0 then -- ->3.2.4.1
+         Z := true;
+      else
+         Z := false;
+      end if;
+
+      -- ->3.2.5.5
+      if A_u(0) = '1' then
+         CO := true;
+      else
+         CO := false;
+      end if;
+
+      -- ->3.2.6.2
+      -- MSB of R is always '0'
+      N := false;
+
+      O := false; -- ->3.2.7.3
+
+      R := R_b;
+
+   end procedure; -- Julian Leyh
+
+
+
+   -- Arithmetic Shift Right
+   procedure EXEC_SRA  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   ) is
+
+      constant A_u   : unsigned (DATA_WIDTH-1 downto 0) := to_unsigned(A,DATA_WIDTH);
+      variable R_b   : integer range 0 to 2**DATA_WIDTH-1 := 0;
+
+   begin
+
+      R_b := to_integer(A_u(DATA_WIDTH-1) & A_u(DATA_WIDTH-1 downto 1));
+
+      if R_b = 0 then -- ->3.2.4.1
+         Z := true;
+      else
+         Z := false;
+      end if;
+
+      -- ->3.2.5.5
+      if A_u(0) = '1' then
+         CO := true;
+      else
+         CO := false;
+      end if;
+
+      -- ->3.2.6.2
+      -- A_u(DATA_WIDTH-1 remains MSB of R
+      if A_u(DATA_WIDTH-1) = '1' then
+         N := true;
+      else
+         N := false;
+      end if;
+
+      O := false; -- ->3.2.7.3
+
+      R := R_b;
+
+   end procedure; -- Julian Leyh
+
+
+
+   -- Rotate Right
+   procedure EXEC_ROR  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   ) is
+
+      constant A_u   : unsigned (DATA_WIDTH-1 downto 0) := to_unsigned(A,DATA_WIDTH);
+      variable R_b   : integer range 0 to 2**DATA_WIDTH-1 := 0;
+
+   begin
+
+      R_b := to_integer(A_u(0) & A_u(DATA_WIDTH-1 downto 1));
+
+      if R_b = 0 then -- ->3.2.4.1
+         Z := true;
+      else
+         Z := false;
+      end if;
+
+      CO := false; -- ->3.2.5.1
+
+      -- ->3.2.6.2
+      -- Since A_u(0) is MSB of R we'll just use A_u(0)
+      -- This should be faster and it's less code
+      if A_u(0) = '1' then
+         N := true;
+      else
+         N := false;
+      end if;
+
+      O := false; -- ->3.2.7.3
+
+      R := R_b;
+
+   end procedure; -- Julian Leyh
+
+
+
+   -- Rotate Right through Carry
+   procedure EXEC_RORC ( A        : in  data_type;
+                         CI       : in  boolean;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   ) is
+
+      constant A_u   : unsigned (DATA_WIDTH-1 downto 0) := to_unsigned(A,DATA_WIDTH);
+      variable R_b   : integer range 0 to 2**DATA_WIDTH-1 := 0;
+
+   begin
+
+      if CI = true then
+        R_b := to_integer('1' & A_u(DATA_WIDTH-1 downto 1));
+      else
+        R_b := to_integer('0' & A_u(DATA_WIDTH-1 downto 1));
+      end if;
+
+      if R_b = 0 then -- ->3.2.4.1
+         Z := true;
+      else
+         Z := false;
+      end if;
+
+      if A_u(0) = '1' then -- ->3.2.5.5
+         CO := true;
+      else
+         CO := false;
+      end if;
+
+      -- ->3.2.6.2
+      -- Since CI is MSB of R we'll just use CI
+      N := CI;
+
+      O := false; -- ->3.2.7.3
+
+      R := R_b;
+
+   end procedure; -- Julian Leyh
+
+
+
+   -- Rotate Left
+   procedure EXEC_ROL  ( A        : in  data_type;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   ) is
+
+      constant A_u   : unsigned (DATA_WIDTH-1 downto 0) := to_unsigned(A,DATA_WIDTH);
+      variable R_b   : integer range 0 to 2**DATA_WIDTH-1 := 0;
+
+   begin
+
+      R_b := to_integer(A_u(DATA_WIDTH-2 downto 0) & A_u(DATA_WIDTH-1));
+
+      if R_b = 0 then -- ->3.2.4.1
+         Z := true;
+      else
+         Z := false;
+      end if;
+
+      CO := false; -- ->3.2.5.1
+
+      -- ->3.2.6.2
+      -- Since A_u(DATA_WIDTH-2) is MSB of R we'll just use A_u(DATA_WIDTH-2)
+      -- This should be faster and it's less code
+      if A_u(DATA_WIDTH-2) = '1' then
+         N := true;
+      else
+         N := false;
+      end if;
+
+      O := false; -- ->3.2.7.3
+
+      R := R_b;
+
+   end procedure; -- Julian Leyh
+
+
+
+   -- Rotate Left through Carry
+   procedure EXEC_ROLC ( A        : in  data_type;
+                         CI       : in  boolean;
+                         R        : out data_type;
+                         Z,CO,N,O : out boolean   ) is
+
+      constant A_u   : unsigned (DATA_WIDTH-1 downto 0) := to_unsigned(A,DATA_WIDTH);
+      variable R_b   : integer range 0 to 2**DATA_WIDTH-1 := 0;
+
+   begin
+
+      if CI = true then
+        R_b := to_integer(A_u(DATA_WIDTH-2 downto 0) & '1');
+      else
+        R_b := to_integer(A_u(DATA_WIDTH-2 downto 0) & '0');
+      end if;
+
+      if R_b = 0 then -- ->3.2.4.1
+         Z := true;
+      else
+         Z := false;
+      end if;
+
+      if A_u(DATA_WIDTH-1) = '1' then -- ->3.2.5.4
+         CO := true;
+      else
+         CO := false;
+      end if;
+
+      -- ->3.2.6.2
+      -- Since A_u(DATA_WIDTH-2) is MSB of R we'll just use A_u(DATA_WIDTH-2)
+      -- This should be faster and it's less code
+      if A_u(DATA_WIDTH-2) = '1' then
+         N := true;
+      else
+         N := false;
+      end if;
+
+      O := false; -- ->3.2.7.3
+
+      R := R_b;
+
+   end procedure; -- Julian Leyh
+
+
 
 end CPU_DEFS_PACK;
+
